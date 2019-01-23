8da8c9b26e5624e276a6951b1c147eef4ffc666c
asudau <sudau@elan-ev.de>
2019-01-23 17:46:22 +0100

    rel #26

--- Studip_VHS.php
+++ Studip_VHS.php
@@ -42,35 +42,40 @@ class Studip_VHS extends StudIPPlugin implements StandardPlugin, SystemPlugin
             'mitarbeiter',
             'index_wesermarsch',
             'index_allgemein');
-            
-        
+           
+        //Falls wir schon in einem Kurs sind, checke ob dieser schon gestartet und konfiguriere die Navigation
+        $referer = $_SERVER['REQUEST_URI'];
         $this->course = Course::findCurrent();
 	 	$this->course_id = $this->course->id;
 		
 		if ($this->course)
 		{
-            $this->setupStudIPNavigation();	
+            if(!$this->course_available($this->course_id) && $referer==str_replace("seminar_main.php","",$referer) && $referer==str_replace("/studip_vhs/seminar","",$referer) ){
+                PageLayout::postMessage(MessageBox::error(_('Dieser Kurs ist noch nicht gestartet.')));
+                header('Location: '. $GLOBALS['ABSOLUTE_URI_STUDIP'] . 'dispatch.php/my_courses?cid=' , false, 303);
+                exit();
+            }
+            $this->setupStudIPNavigation($this->course_id);	
         }
         
-        //setup intranet navigation and forward if just logged in
-        //TOTO: auslagern
-        $intranets = $this->getIntranetIDsForUser();
-        
-        if (Navigation::hasItem('/start') && $intranets){
-            Navigation::getItem('/start')->setURL(PluginEngine::getLink($this, array(), 'intranet_start/index/' . $intranets[0]) );
-        }
-        
+
+        //Adminbereich für Intranetverwaltung
         if($perm->have_perm('root')){
             $navigation = new Navigation('Intranetverwaltung', PluginEngine::getURL($this, array(), 'intranetverwaltung/index'));
             $navigation->addSubNavigation('index', new Navigation('Übersicht', PluginEngine::getURL($this, array(), 'intranetverwaltung/index')));
             $navigation->addSubNavigation('settings', new Navigation('Einstellungen', PluginEngine::getURL($this, array(), 'settings')));
             Navigation::addItem('/admin/intranetverwaltung', $navigation);
         } 
-
-        $referer = $_SERVER['REQUEST_URI'];
    
+        //setup intranet navigation and forward if just logged in
+        //TODO: auslagern
+        $intranets = $this->getIntranetIDsForUser();
+        
+        if (Navigation::hasItem('/start') && $intranets){
+            Navigation::getItem('/start')->setURL(PluginEngine::getLink($this, array(), 'intranet_start/index/' . $intranets[0]) );
+        }
         //Intranetnutzer werden statt auf die allgemeine Startseite auf ihre individuelle Startseite weitergeleitet
-        if ( $referer!=str_replace("dispatch.php/start","",$referer) &&  $intranets){;
+        if ( $referer!=str_replace("dispatch.php/start","",$referer) &&  $intranets){
             //$result = $this->getSemStmt($GLOBALS['user']->id);
             header('Location: '. PluginEngine::getLink($this, array(), 'intranet_start/index/' . $intranets[0]) , false, 303);
             exit();	
@@ -97,7 +102,7 @@ class Studip_VHS extends StudIPPlugin implements StandardPlugin, SystemPlugin
         
         //Kurs hat noch nicht begonnen
         //TODO Navigation deaktivieren und Fehler werfen in den anderen Actions
-        if (!$GLOBALS['perm']->have_studip_perm('tutor', $course_id) && $localEntries[$this->datafield_id_begin]->value > time()){
+        if (!$this->course_available($course_id)){
             $navigation = new Navigation(_('Übersicht'));
             $navigation->setImage(Icon::create('seminar', 'info_alt'));
             $navigation->setActiveImage(Icon::create('seminar', 'info'));
@@ -188,55 +193,80 @@ class Studip_VHS extends StudIPPlugin implements StandardPlugin, SystemPlugin
         return $intranets;
     }
     
-    private function setupStudIPNavigation(){
+    private function setupStudIPNavigation($course_id){
 		
         //falls individuelle Einstellungen zur Reihenfogle vorliegen
 		$block = SeminarTab::findOneBySQL('seminar_id = ? ORDER BY position ASC',
-                                 array($this->course_id) );
-		if($block){
-			$this->sortCourseNavigation();
-		}
-
-    }
-	
-    private function sortCourseNavigation(){
-	global $perm;
-   	$restNavigation = array();
-	$newNavigation = Navigation::getItem('/course');
-	foreach(Navigation::getItem('/course') as $key => $tab){
-		$block = SeminarTab::findOneBySQL('seminar_id = ? AND tab IN (?) ORDER BY position ASC',
-                                 array($this->course_id,$key) );
-		if($block){
-			$tab->setTitle($block->getValue('title'));
-			if($block->getValue('tn_visible') == true || $perm->have_studip_perm('dozent', Request::get('cid')) ){
-				$subNavigations[$block->getValue('position')][$key] = $tab;
-			}
-					
-		} else { 
-		   //keine Info bezüglich Reihenfolge also hinten dran
-		   //greift bei neu aktivierten Navigationselementen
-		   $restNavigation[$key] = $tab;
-
-		}
-
-		$newNavigation->removeSubNavigation($key);
-	}	
-	
-	ksort($subNavigations);
-
-	foreach($subNavigations as $subNavs){
-	    foreach($subNavs as $key => $subNav){
-		$newNavigation->addSubNavigation($key, $subNav);
-		
-	    }
-	}
-	if(count($restNavigation)>0){
-        foreach($restNavigation as $key => $restNav){
-            $newNavigation->addSubNavigation($key, $restNav);  
+                                 array($course_id) );
+        if (!$this->course_available($course_id)){
+            $this->clearCourseNavigation($course_id);
         }
-	}
-
-	Navigation::addItem('/course', $newNavigation);
+		else if($block){
+			$this->sortCourseNavigation($course_id);
+		}
     }
+	
+    private function sortCourseNavigation($course_id){
+        global $perm;
+        $restNavigation = array();
+        $newNavigation = Navigation::getItem('/course');
+        foreach(Navigation::getItem('/course') as $key => $tab){
+            $block = SeminarTab::findOneBySQL('seminar_id = ? AND tab IN (?) ORDER BY position ASC',
+                                     array($this->course_id,$key) );
+            if($block){
+                $tab->setTitle($block->getValue('title'));
+                if($block->getValue('tn_visible') == true || $perm->have_studip_perm('dozent', Request::get('cid')) ){
+                    $subNavigations[$block->getValue('position')][$key] = $tab;
+                }
+            } else { 
+               //keine Info bezüglich Reihenfolge also hinten dran
+               //greift bei neu aktivierten Navigationselementen
+               $restNavigation[$key] = $tab;
+            }
+            $newNavigation->removeSubNavigation($key);
+        }	
+
+        ksort($subNavigations);
+
+        foreach($subNavigations as $subNavs){
+            foreach($subNavs as $key => $subNav){
+                $newNavigation->addSubNavigation($key, $subNav);
+            }
+        }
+        if(count($restNavigation)>0){
+            foreach($restNavigation as $key => $restNav){
+                $newNavigation->addSubNavigation($key, $restNav);  
+            }
+        }
+        Navigation::addItem('/course', $newNavigation);
+    }
+    
+     private function clearCourseNavigation(){
+        $navigation = Navigation::getItem('/course');
+        $overview = '';
+        
+        //diese unterscheidung muss anders gemacht werden
+        foreach(Navigation::getItem('/course') as $key => $tab){
+            $navigation->removeSubNavigation($key);
+        }    
+        $new_navigation = new Navigation(_('Übersicht'));
+        $new_navigation->setImage(Icon::create('seminar', 'info_alt'));
+        $new_navigation->setActiveImage(Icon::create('seminar', 'info'));
+        $new_navigation->setURL(PluginEngine::getURL($this, [], 'seminar/not_started'));
+        $navigation->addSubNavigation('overview_vhs', $new_navigation);
+        Navigation::addItem('/course', $navigation);
+     }
+     
+     private function course_available($course_id){
+        $localEntries = DataFieldEntry::getDataFieldEntries($course_id);
+        
+        $datafield_begin =  DataField::findOneBySQL('name = \'course begin\'');
+        $this->datafield_id_begin = $datafield_begin->datafield_id;
+        
+        //falls Kurs noch nciht gestartet und Nutzer autor -> noch kein Zugriff
+        if ((!$GLOBALS['perm']->have_studip_perm('tutor', $course_id) || $localEntries[$this->datafield_id_begin]->value > time())){
+            return false;
+        }else return true;
+     }
     
 }
