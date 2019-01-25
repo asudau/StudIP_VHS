4a635d45fe660d08d632c2e32d8ca6242f044cdc
asudau <sudau@elan-ev.de>
2019-01-25 15:58:34 +0100

    fix

--- views/intranet_start/index_ammerland.php
+++ views/intranet_start/index_ammerland.php
@@ -87,7 +87,7 @@
                     </h2>
                     <? foreach ($folderwithfiles as $folder => $files): ?>
                     <section class="contentbox folder">
-                        <a class='folder_open' href=''><?= $folder ?></a>
+                        <a class='folder_open' href=''><?= DocumentFolder::find($folder)->name ?></a>
                         <? foreach ($files as $file): ?>
                         <li class='file_download' style="display:none"> <a href='<?=$GLOBALS['ABSOLUTE_URI_STUDIP']?>sendfile.php?force_download=1&type=0&file_id=<?= $file['dokument_id']?>&file_name=<?= $file['filename'] ?>'><?= $file['name'] ?></a></li>
                         
