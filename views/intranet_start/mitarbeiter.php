93d2ae31a2a33ca931e0ef71cdaea87b48d69827
asudau <sudau@elan-ev.de>
2019-01-29 11:25:48 +0100

    rel #28

--- views/intranet_start/mitarbeiter.php
+++ views/intranet_start/mitarbeiter.php
@@ -154,19 +154,31 @@
 			</div>
 	<div class="haupt">
 
-        <div style='width: 400px; margin:auto'>
+        <div style='width: 100%; margin:auto; padding-bottom: 10px'>
             <table cellspacing="0" cellpadding="0" border="0">
                 <tbody><tr>
-                    <td class="dsR4"><div class="zentriert">
-                        <a data-dialog="title=Feedback geben;width=300px; height=200px;" href="<?=$this->controller->url_for('intranet_start/feedback_chat')?>" title="Feedback"><?= Icon::create('chat2', 'clickable', ['size' => 100])?></a>
+                    <td class="dsR4"><div class="zentriert intranet-kachel">
+                        <a data-dialog="title=Feedback geben;width=300px; height=200px;" href="<?=$this->controller->url_for('intranet_start/feedback_chat')?>" title="Feedback">
+                            <?= Icon::create('chat2', 'clickable', ['size' => 100])?>
+                        <br>
+                            Feedback
+                        </a>
                     </div>
                     </td>
-                    <td class="dsR4"><div class="zentriert">
-                        <a data-dialog href="<?=$this->controller->url_for('intranet_start/feedback_form')?>" title="Störungsmeldung" ><?= Icon::create('exclaim-circle-full', 'clickable', ['size' => 100])?></a>
+                    <td class="dsR4"><div class="zentriert intranet-kachel">
+                        <a data-dialog href="<?=$this->controller->url_for('intranet_start/feedback_form')?>" title="Störungsmeldung" >
+                            <?= Icon::create('exclaim-circle-full', 'clickable', ['size' => 100])?>
+                            <br>
+                            Störungsmeldung
+                        </a>
                     </div>
                                 </td>
-                    <td class="dsR4"><div class="zentriert">
-                        <a href="https://www.kvhs-ammerland.de/fileadmin/user_upload/Programmheft/6_Dozentenfortbildungen.pdf" target='_blank' title="Zu den Dozentenfortbildungen auf unserer Homepage" target='_blank'><?= Icon::create('doctoral_cap', 'clickable', ['size' => 100])?></a>
+                    <td class="dsR4"><div class="zentriert intranet-kachel">
+                        <a href="https://www.kvhs-ammerland.de/fileadmin/user_upload/Programmheft/6_Dozentenfortbildungen.pdf" target='_blank' title="Zu den Dozentenfortbildungen auf unserer Homepage" target='_blank'>
+                            <?= Icon::create('doctoral_cap', 'clickable', ['size' => 100])?>
+                        <br>
+                            Fortbildungen
+                        </a>
                     </div>
                     </td>
                     </tr>
