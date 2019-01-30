1af62916464ec5eaf3db110f4075c262821020e0
asudau <sudau@elan-ev.de>
2019-01-30 12:59:15 +0100

    fix #20

---
+++ views/intranetverwaltung/index/members.php
@@ -0,0 +1,32 @@
+<? if ($inst): ?>
+
+<h1> NutzerInnen: <?= $inst->name ?></h1>
+<table id="keywordstt" class="sortable-table default">
+    <thead>
+		<tr>
+        <th data-sort="text"><span>Username</span></th>
+        <th data-sort="text"><span>Name</span></th>
+        <th data-sort="text"><span>eMail</span></th>
+        <th data-sort="text" style=''><span>Status</span></th>  
+        <th><span>Aktionen</span></th>
+    </tr>
+    </thead>
+    
+    <tbody>
+    
+    <?php foreach ($members as $member): ?>
+        <tr>
+            <td><a href='<?= URLHelper::getLink('dispatch.php/profile', ['username' => $member->username]) ?>'><?= $member->username ?></a></td>
+            <td><?= $member->vorname . ' ' . $member->nachname?></a></td>
+            <td><?= $member->email ?></td>
+            <td><?= ($member->inst_perms == 'dozent')? 'Intranet-Administrator' : 'Intranet Mitglied' ?></a></td>
+            <td>
+                <a title='(Nochmal) Einladen' href = <?= $controller->url_for('index/send_register_invitation/' . $member->user_id )?>> <?=Icon::create('mail', 'clickable')?>
+            </td>
+        </tr>
+    <?php endforeach ?>
+        
+    </tbody>
+</table>
+
+<? endif; ?>
