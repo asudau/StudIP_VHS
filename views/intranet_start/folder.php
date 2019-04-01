797a1cec540da565727e7225cb68989169b9012d
asudau <sudau@elan-ev.de>
2019-04-01 14:40:52 +0200

    debugging

--- views/intranet_start/folder.php
+++ views/intranet_start/folder.php
@@ -1,2 +1,3 @@
 
+<?var_dump($folderwithfiles);?>
 <?= $this->render_partial('_partials/folder_with_files', array('folderwithfiles' => $folderwithfiles, 'parentfolder' => $parentfolder, 'parent' => NULL, 'display' => 'block')) ?>
