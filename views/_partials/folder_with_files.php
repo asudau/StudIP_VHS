25fe92962a16c5c9bdfc50586ead9d3aebd91192
asudau <sudau@elan-ev.de>
2020-02-21 12:20:03 +0100

    fix #68

--- views/_partials/folder_with_files.php
+++ views/_partials/folder_with_files.php
@@ -5,14 +5,13 @@
         <? if(array_keys($parentfolder, $folder)) : ?>
             <? foreach (array_keys($parentfolder, $folder) as $subfolder): ?>
                 <?= $this->render_partial('_partials/folder_with_files', 
-                    array('folderwithfiles' => [$subfolder => $folderwithfiles[$subfolder]], 
+                    array('folderwithfiles' => $folderwithfiles, 
                         'parentfolder' => $parentfolder,
                         'parent' => $folder)) ?>
             <? endforeach ?>
         <? endif ?>
         <? foreach ($files as $file): ?>
             <li class='file_download' style="display: <?=$display? : 'none' ?> "> <a href='<?=$GLOBALS['ABSOLUTE_URI_STUDIP']?>sendfile.php?force_download=1&type=0&file_id=<?= $file['id']?>&file_name=<?= $file['filename'] ?>'><?= $file['name'] ?></a></li>
-
         <? endforeach ?>
         </section>
     <? endif ?>
