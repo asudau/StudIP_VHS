cd013c5d5e36e6fe7ebe2342fdf1ba844f2d1a51
asudau <sudau@elan-ev.de>
2020-11-08 12:32:39 +0100

    fix migration

--- migrations/012_Intranet_individual_buttons.php
+++ migrations/012_Intranet_individual_buttons.php
@@ -23,10 +23,11 @@ class IntranetIndividualButtons extends Migration
 
         $db->exec("CREATE TABLE IF NOT EXISTS `intranet_button_content` (
           `Button_id` varchar(32) NOT NULL,
+          `Content_id` varchar(32) NOT NULL,
           `position` int(11),
           `target` varchar(255),
           `icon` varchar(255),
-          PRIMARY KEY (Institut_id)
+          PRIMARY KEY (Content_id)
         ) ");
 
         SimpleORMap::expireTableScheme();
